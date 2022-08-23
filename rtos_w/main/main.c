7b76ce70bb5647e0983416f7233013313b90a8e4
fateh-shahrukh-cowlar <fateh.shahrukh.cowlar@gmail.com>
2022-08-23 15:52:55 +0500

    task notification 1

--- rtos_w/main/main.c
+++ rtos_w/main/main.c
@@ -3,25 +3,25 @@
 #include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-
+static TaskHandle_t receievrHandler=NULL;
 void sender(void *params)
 {
     while(true)
     {
-        xtaskNotifyGive(recieverHandler);
+        xTaskNotifyGive(receievrHandler);
         vTaskDelay(5000/portTICK_PERIOD_MS);
     }
 }
-void reciever(void *params)
+void receiver(void *params)
 {
     while(true)
     {
         int count=ulTaskNotifyTake(pdFALSE,portMAX_DELAY);
-        printf("Recieved Notification %d times /n",count);
+        printf("Recieved Notification %d times \n",count);
     }
 }
 void app_main(void)
 {
-    xTaskCreate(&reciever,"sender",2048,NULL,2,NULL);
+    xTaskCreate(&receiver,"sender",2048,NULL,2,&receievrHandler);
     xTaskCreate(&sender,"reciever",2048,NULL,2,NULL);
 }
