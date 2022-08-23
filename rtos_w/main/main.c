9bbd47f8334ba34b8e69e091557a78e113797a11
fateh-shahrukh-cowlar <fateh.shahrukh.cowlar@gmail.com>
2022-08-23 15:33:29 +0500

    tasks

--- rtos_w/main/main.c
+++ rtos_w/main/main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
+void sender(void *params)
+{
+    while(true)
+    {
+
+    }
+}
+void sender(void *params)
+{
+    while(true)
+    {
+        
+    }
+}
 void app_main(void)
 {
-
+    xTaskCreate(&reciever,"sender",2048,NULL,2,NULL);
+    xTaskCreate(&sender,"reciever",2048,NULL,2,NULL);
 }
